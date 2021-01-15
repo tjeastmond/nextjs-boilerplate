33efdeea70d0149be976998ae3f20926b09569a4
TJ Eastmond <tj.eastmond@gmail.com>
2021-01-15 15:33:34 -0500

    Updated packages

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,7 @@ export default function Home() {
         <title>Next.js Bootstrap</title>
       </Head>
       <h1>Next.js Bootstrap</h1>
-      <p>A simple Next.js Bootstrap for getting your POCs up and running quickly!</p>
-      <p>I have setup or already included a bunch of stuff up already: Prettier, Material-UI, Husky, and Axios.</p>
-      <p>Have fun.</p>
+      <p>A simple Next.js Bootstrap mostly for me</p>
     </Container>
   );
 }
